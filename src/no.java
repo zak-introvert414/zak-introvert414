6779a7509e7d133f43ff74eda032bf4744434d75
zak-introvert414 <zeshank414@gmail.com>
2022-05-19 17:36:35 +0500

    sixth  commit

--- src/no.java
+++ src/no.java
@@ -2,5 +2,6 @@ public class no {
     public static void main(String[] args) {
         System.out.println("you are very cute ");
         System.out.println("this is all for the testing purposes");
+        System.out.println("it will take time to be in a goo from");
     }
 }
