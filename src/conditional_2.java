ade4a893ec3887180a653fb0caaf2adbf7782e3f
zak-introvert414 <zeshank414@gmail.com>
2022-05-19 17:11:56 +0500

    intital commit

---
+++ src/conditional_2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class conditional_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("you are adult");
+        } else {
+            System.out.println("you`r not an adult");
+        }
+
+
+    }
+
+    }
+
